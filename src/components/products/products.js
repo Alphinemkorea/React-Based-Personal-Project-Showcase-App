2428dc89315513e1d11c826d5890bdd3c1116269
Alphine <alphinemkorea3@gmail.com>
2026-05-15 11:50:10 +0300

    Updated products data

--- src/components/products/products.js
+++ src/components/products/products.js
@@ -3,8 +3,7 @@ export const products = [
         id: 1,
         name: "Gaming PC RTX Build",
         price: 1200,
-        description:
-            "High-performance gaming PC for AAA games.",
+        description: "High-performance gaming PC for AAA games.",
         image:
             "https://www.deltarentals.com.au/wp-content/uploads/2022/01/Custom-Builder-scaled.webp",
     },
@@ -13,8 +12,7 @@ export const products = [
         id: 2,
         name: "NVIDIA RTX 4070 GPU",
         price: 650,
-        description:
-            "Powerful graphics card for gaming.",
+        description: "Powerful graphics card for gaming.",
         image:
             "https://i5.walmartimages.com/seo/PNY-GeForce-RTX-4070-GPU-12GB-XLR8-Gaming-VERTO-EPIC-X-RGB-Triple-Fan-DLSS-3-Graphics-Card_b9868c62-2285-42c1-9398-63b962c541a6.e565daea43daff5166d094180e2ab61b.jpeg",
     },
@@ -23,8 +21,7 @@ export const products = [
         id: 3,
         name: "AMD Ryzen 7 CPU",
         price: 320,
-        description:
-            "Fast gaming processor for multitasking.",
+        description: "Fast gaming processor for multitasking.",
         image:
             "https://gamersarenacustompc.co.ke/wp-content/uploads/2023/10/19-113-567-V11.jpg",
     },
@@ -33,9 +30,107 @@ export const products = [
         id: 4,
         name: "Liquid CPU Cooler",
         price: 120,
-        description:
-            "Keeps your gaming PC cool.",
+        description: "Keeps your gaming PC cool.",
         image:
             "https://www.enermax.com/archive/products/item/LiquidCooler/AQUAFUSION/360/AQUAFUSION_360_001.jpg",
+    },
+
+    {
+        id: 5,
+        name: "Mechanical RGB Keyboard",
+        price: 95,
+        description: "RGB mechanical keyboard with fast response.",
+        image:
+            "https://starmac.co.ke/wp-content/uploads/2025/10/Razer-BlackWidow-V4-RGB-Mechanical-Gaming-Keyboard.jpg",
+    },
+
+    {
+        id: 6,
+        name: "Gaming Mouse Pro",
+        price: 60,
+        description: "High precision gaming mouse with RGB lights.",
+        image:
+            "https://m.media-amazon.com/images/I/61mpMH5TzkL.jpg",
+    },
+
+    {
+        id: 7,
+        name: "UltraWide Gaming Monitor",
+        price: 450,
+        description: "144Hz ultrawide monitor for immersive gaming.",
+        image:
+            "https://sm.pcmag.com/pcmag_uk/guide/t/the-best-u/the-best-ultrawide-monitors-for-2026_s724.jpg",
+    },
+
+    {
+        id: 8,
+        name: "Gaming Headset X",
+        price: 85,
+        description: "Surround sound headset with noise cancellation.",
+        image:
+            "https://m.media-amazon.com/images/I/71BKQhFzDmL.jpg",
+    },
+
+    {
+        id: 9,
+        name: "PlayStation 5 Console",
+        price: 700,
+        description: "Next-gen console for high performance gaming.",
+        image:
+            "https://m.media-amazon.com/images/I/619BkvKW35L.jpg",
+    },
+
+    {
+        id: 10,
+        name: "Xbox Series X",
+        price: 680,
+        description: "Powerful gaming console with 4K support.",
+        image:
+            "https://media.wired.com/photos/5fa5dc3dba670daaf8e97a8d/191:100/w_1280,c_limit/games_gear_series-x.jpg",
+    },
+
+    {
+        id: 11,
+        name: "Gaming Chair Elite",
+        price: 250,
+        description: "Comfortable ergonomic gaming chair.",
+        image:
+            "https://www.brazengamingchairs.com/cdn/shop/files/BraZenSentinelRedB2B-1_800x.jpg?v=1717665228",
+    },
+
+    {
+        id: 12,
+        name: "Streaming Microphone RGB",
+        price: 140,
+        description: "Crystal clear microphone for gaming streams.",
+        image:
+            "https://images-na.ssl-images-amazon.com/images/I/71CKdnpt2LL.jpg",
+    },
+
+    {
+        id: 13,
+        name: "Gaming Desk ",
+        price: 300,
+        description: "Modern gaming desk with RGB lighting.",
+        image:
+            "https://furniturevillage.co.ke/wp-content/uploads/2026/02/ddb0b895-d5dc-4e22-b75d-cdf39bb4f0b1-1000x750-KpmaFUQ7bfLy2VleA8plW7QaNrfg6anDo2LMuq7A.jpg",
+    },
+
+    {
+        id: 14,
+        name: "DualSense Wireless Controller",
+        price: 75,
+        description: "Wireless controller with adaptive triggers.",
+        image:
+            "https://m.media-amazon.com/images/I/61CGHv6kmWL.jpg",
+    },
+
+    {
+        id: 15,
+        name: "VR Gaming Headset",
+        price: 500,
+        description: "Immersive virtual reality gaming experience.",
+        image:
+            "https://m.media-amazon.com/images/I/61kwRNPtMpL.jpg",
     },
 ];
